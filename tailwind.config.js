2fe2079604f98d21963c60b7e19152cae1903f07
Pedro Gonçalves <pedrofgs91@gmail.com>
2023-08-02 10:11:55 +0200

    Set up base colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "theme-yellow": "#F3C615",
+        "theme-pink": "#E95B9A",
+        "theme-gray": "#5A5A5A",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
